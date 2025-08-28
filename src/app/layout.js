59654a22a7d48373a5edca75f7030d1e185d9d6a
YoussufSalah <youssufsalahsaber@gmail.com>
2025-08-28 17:29:28 +0300

    Added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter, JetBrains_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -45,6 +46,7 @@ export default function RootLayout({ children }) {
                 className={`${inter.variable} ${jetbrains.variable} antialiased`}
             >
                 {children}
+                <Analytics />
             </body>
         </html>
     );
